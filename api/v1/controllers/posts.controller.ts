1c03baa2040aadd59e5904aaf52ba260c35b85bd
vanphuTteam <vanphu120107@gmail.com>
2026-05-01 22:33:09 +0700

    API delete comment post

--- api/v1/controllers/posts.controller.ts
+++ api/v1/controllers/posts.controller.ts
@@ -410,6 +410,107 @@ export const postComments = async (req: ExtendRequest, res: Response) => {
     }
 }
 
+// [DELETE] /posts/:postId/comments/:commentId
+export const deleteComments = async (req: ExtendRequest, res: Response) => {
+    try {
+        const userId = req.user?.id;
+        const postId = req.params?.postId;
+        const commentId = req.params?.commentId;
+
+
+        // Kiểm tra đăng nhập
+        if (!userId) {
+            res.status(401).json({
+                success: false,
+                error: "Vui lòng đăng nhập!"
+            });
+            return;
+        }
+
+        // Kiểm tra có truyền postId lên params không
+        if (!postId) {
+            res.status(400).json({
+                success: false,
+                error: "Vui lòng gửi kèm postId lên params!"
+            });
+            return;
+        }
+
+        // Kiểm tra postId có hợp lệ không
+        if (!mongoose.isValidObjectId(postId)) {
+            res.status(400).json({
+                success: false,
+                error: "postId gửi lên params không hợp lệ!"
+            });
+            return;
+        }
+
+        // Kiểm tra bài viết có tồn tại
+        const isPost = await Post.exists({ _id: postId });
+        if (!isPost) {
+            res.status(404).json({
+                success: false,
+                error: "Bài viết không tồn tại!"
+            });
+            return;
+        }
+
+        // Kiểm tra có truyền commentId lên params không
+        if (!commentId) {
+            res.status(400).json({
+                success: false,
+                error: "Vui lòng gửi kèm commentId lên params!"
+            });
+            return;
+        }
+
+        // Kiểm tra commentId có hợp lệ không
+        if (!mongoose.isValidObjectId(commentId)) {
+            res.status(400).json({
+                success: false,
+                error: "CommentId gửi lên params không hợp lệ!"
+            });
+            return;
+        }
+
+        // Kiểm tra xem comment có thuộc post không, nếu có thì xóa luôn
+        const comment = await Comment.findOneAndDelete({
+            _id: commentId,       
+            targetId: postId, 
+            targetType: "post",
+            author: userId
+        }, { new: true}).lean();
+        if (!comment) {
+            res.status(400).json({
+                success: false,
+                error: "Comment không có trong post này!"
+            });
+            return;
+        }
+
+        // Update cho comment và commentCount cho posts
+        const post = await Post.findByIdAndUpdate(postId, {
+            $pull: {comments: commentId},
+            $inc: {commentsCount: -1}
+        });
+
+        // Trả data cho client
+        res.status(200).json({
+            success: true,
+            data: {"message": "Comment deleted"}
+        });
+        return;
+
+    } catch (error) {
+        console.log("[Post comment Error: ]", error);
+        res.status(500).json({
+            success: false,
+            error: "Lỗi hệ thống, vui lòng thử lại sau!"
+        });
+        return;
+    }
+}
+
 // [POST] /posts/:postId/comments/:commentId/like
 export const likeCommentPost = async (req: ExtendRequest, res: Response) => {
     try {
