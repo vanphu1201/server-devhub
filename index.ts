7a87573a9433087983684f44ff5aefff05f63628
vanphuTteam <vanphu120107@gmail.com>
2026-05-29 21:48:16 +0700

    Configure server: tich hop CORS va cau hinh build/start script cho deploy Render

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express, { Express, Request, Response} from "express";
+import cors from "cors";
 import { connect } from "./configs/database";
 import mainV1Route from "./api/v1/routes/index.route";
 
@@ -8,6 +9,7 @@ import mainV1Route from "./api/v1/routes/index.route";
 const app: Express= express();
 connect();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
